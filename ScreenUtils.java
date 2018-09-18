33b5b0e227048a41047623908f2c12095134d270
Hasan Mhd Amin <hasan.mhd.amin@gmail.com>
2018-09-18 12:21:47 +0200

    create screen utils

---
+++ ScreenUtils.java
@@ -0,0 +1,21 @@
+package info.interactivesystems.pax.moodtracker.utils;
+
+import android.app.Activity;
+import android.view.WindowManager;
+
+/**
+ * Created by Hasan Mhd Amin on 9/18/2018.
+ */
+
+public class ScreenUtils {
+
+
+    public static void setStatusBarTranslucent(Activity context, boolean makeTranslucent) {
+        if (makeTranslucent) {
+            context.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        } else {
+            context.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+    }
+
+}
