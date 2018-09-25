f422d763c6c8f69aca58ef94162237fc998f6bf4
Hasan Mhd Amin <hasan.mhd.amin@gmail.com>
2018-09-25 14:47:28 +0200

    update screen utils

--- ScreenUtils.java
+++ ScreenUtils.java
@@ -1,5 +1,10 @@
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Point;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.WindowManager;
 
 /**
@@ -16,5 +21,39 @@ public class ScreenUtils {
             context.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
     }
+
+
+    public static int convertDpToPixel(float dp, Context context) {
+        Resources resources = context.getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        Float px = dp * (metrics.densityDpi / 160f);
+        return px.intValue();
+    }
+
+    public static int convertPixelToDp(float px, Context context){
+        Resources resources = context.getResources();
+        DisplayMetrics metrics = resources.getDisplayMetrics();
+        float dp = px / ((float)metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+        return (int) dp;
+    }
+
+    public static int getScreenHeight(Activity activity) {
+        Display display = activity.getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int height = size.y;
+
+        return height;
+    }
+
+    public static int getScreenWidth(Activity activity) {
+        Display display = activity.getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+
+        return width;
+    }
+
 
 }
