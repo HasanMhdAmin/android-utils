83dac1054bf38a5276538fbf566a42191c9310d6
Hasan Mhd Amin <hasan.mhd.amin@gmail.com>
2018-10-02 14:18:40 +0200

    add set status bar color

--- ScreenUtils.java
+++ ScreenUtils.java
@@ -22,6 +22,18 @@ public class ScreenUtils {
         }
     }
 
+    public static void setStatusBarColor(Activity context, int color) {
+        Window window = context.getWindow();
+
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+
+        // finally change the color
+        window.setStatusBarColor(color);
+    }
 
     public static int convertDpToPixel(float dp, Context context) {
         Resources resources = context.getResources();
